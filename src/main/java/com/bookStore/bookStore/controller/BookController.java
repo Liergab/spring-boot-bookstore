9fbadea1185ea1bd320cda4cad4c683a6a063302
Bryan Gabriel <bryangabrielberja25@gmail.com>
2024-05-18 20:09:01 +0800

    mvc implement

--- src/main/java/com/bookStore/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/bookStore/controller/BookController.java
@@ -1,14 +1,23 @@
 package com.bookStore.bookStore.controller;
 import com.bookStore.bookStore.entity.Book;
+import com.bookStore.bookStore.entity.MyBookList;
 import com.bookStore.bookStore.service.BookService;
+import com.bookStore.bookStore.service.MyBookListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller //put annotation to know that this class belong to controller
 public class BookController {
     @Autowired
     private BookService service;
+
+    @Autowired
+    private MyBookListService myBookService;
 
     @GetMapping("/")
     public String Home(){
@@ -20,15 +29,29 @@ public class BookController {
         return "bookRegister";
     }
 
-
     @GetMapping("/available_books")
-    public  String getAllBooks(){
-        return "bookList";
+    public ModelAndView getAllBooks(){
+       List<Book> list = service.getAllBooks();
+       return new ModelAndView("bookList","book", list);
+    }
+
+    @GetMapping("/my_books")
+    public ModelAndView getMyBooks(){
+        List<MyBookList> list = myBookService.getAllMyBooks();
+        return new ModelAndView("myBooks","book", list);
     }
 
     @PostMapping("/save")
     public  String addBook(@ModelAttribute Book b){
         service.save(b);
         return "redirect:/available_books";
+    }
+
+    @RequestMapping("/mylist/{id}")
+    public String getMyList(@PathVariable("id") int id){
+        Book b = service.getBookById(id);
+        MyBookList mb = new MyBookList(b.getId(), b.getName(), b.getAuthor(), b.getPrice());
+        myBookService.saveMyBook(mb);
+        return "redirect:/my_books";
     }
 }
---
+++ src/main/java/com/bookStore/bookStore/service/MyBookListService.java
@@ -0,0 +1,23 @@
+package com.bookStore.bookStore.service;
+
+import com.bookStore.bookStore.entity.MyBookList;
+import com.bookStore.bookStore.repository.MyBookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MyBookListService {
+    @Autowired
+    private MyBookRepository MyBook;
+
+    public void saveMyBook(MyBookList book ){
+        MyBook.save(book);
+    }
+
+    public List<MyBookList> getAllMyBooks(){
+        return MyBook.findAll();
+    }
+
+}
